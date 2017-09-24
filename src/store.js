cdc606df3b2b997bbe467956cfda6d6f81a20042
MedinaGitHub <seba.medinad@gmail.com>
2017-09-23 22:20:19 -0300

    ordenando store con combineReducers

--- src/store.js
+++ src/store.js
@@ -1,35 +1,34 @@
-import {createStore , applyMiddleware} from 'redux';
+import {createStore , applyMiddleware , combineReducers} from 'redux';//combineReducers cada funcion se encarga de cierta parte dle estado
 
 import thunk from 'redux-thunk';//para llamadas asincronas
 
-const reduce = (state , action) => {
+const products = (state=[] , action) => {//Combine reducer solo manda parte del estaod segun la configuracion que le hagamos abajo
 
   if(action.type === "REPLACE_PRODUCTS"){
-    return {
-      ...state,
-      products: action.products
-    };
+    return action.products;
   }
 
-  else if(action.type === "ADD_TO_CART"){
-    console.log('add');
-    return  {
-      ...state,
-      cart:state.cart.concat(action.product)
+  /*es posible que con al ejecucion de add to cart tengamos que trabajar products  y ponemos
+    else if(action.type === "ADD_TO_CART"){} como sacar algo del invenetario etc.
+  */
+
+  return state;
+}
+
+const cart = (state=[] , action) => {
+
+  if(action.type === "ADD_TO_CART"){
+      console.log('add');
+      return  state.concat(action.product);
     }
-  }
-
   else if(action.type === "REMOVE_FROM_CART"){
     console.log('remove');
-
-    return {
-      ...state,
-      cart :state.cart.filter( c => c.id !== action.product.id)
-    }
+    return  state.filter( c => c.id !== action.product.id);
   }
 
   return state;
 }
+
 
 const logger = store => next => action => {
   console.log('dispatching', action)
@@ -38,4 +37,9 @@ const logger = store => next => action => {
   return result;
 }
 
-export default createStore(reduce , {cart : [] , products: [] } , applyMiddleware(logger,thunk));
+//combineReducers RECIBE UN OBJETO EN LA QUE CADA LLAVE DEFINE LA FUNCION QUE MANEJARA EL ESTADO SEGUN LA LLAVE QUE LE PONGAMOS,
+//por ejemplo todo lo relacionado con cart, lo maneja el const cart. entonces en state=[] ya no recibe todo el state sino lo que venga como llave
+//ya sea cart o products luego retorna lo que venga en product o cart
+// cpmbone reducer ignora el segundo parametro ({cart : [] , products: [] }) por eso ponermos un parametro opcional o por defecto
+
+export default createStore(combineReducers({cart, products}) , {cart : [] , products: [] } , applyMiddleware(logger,thunk));
